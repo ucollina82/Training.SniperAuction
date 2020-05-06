bdb73750fa29238d4c8b0dddb05bd198614e87b9
umberto <ucollina82@gmail.com>
2020-05-06 11:53:05 +0200

    pulito classe test di partenza

--- src/Training.SniperAuction.Tests/UnitTest1.cs
+++ src/Training.SniperAuction.Tests/UnitTest1.cs
@@ -5,28 +5,14 @@ namespace Training.SniperAuction.Tests
     [TestFixture]
     public class AuctionSniperEndToEndTests
     {
-        //private FakeAuctionServer auction = new FakeAuctionServer("item-54321");
-        //private ApplicationRunner application = new ApplicationRunner();
-
         [SetUp]
         public void Setup()
         {
         }
 
-        //[Test]
-        //public void sniperJoinsAuctionUntilAuctionCloses()
-        //{
-        //    //auction.startSellingItem();
-        //    //application.startBiddingIn(auction);
-        //    //auction.announceClosed();
-        //    //application.showsSniperHasLostAuction();
-        //}
-
         [TearDown]
         private void cleanup()
         {
-            //auction.stop();
-            //application.stop();
         }
     }
 }
